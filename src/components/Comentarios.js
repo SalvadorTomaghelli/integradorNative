781b9bcb93be2a45533b454f240da11ed2d01e56
SalvadorTomaghelli <stomaghelli@udesa.edu.ar>
2024-11-25 13:31:54 -0300

    Likes y borrado sin css

--- src/components/Comentarios.js
+++ src/components/Comentarios.js
@@ -1,25 +1,107 @@
 import React, { Component } from "react";
-import { View, Text, Image, StyleSheet, TextInput } from "react-native";
-import { TouchableOpacity } from "react-native";
-import { db, auth } from "../firebase/config";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { auth, db } from "../firebase/config";
+import firebase from "firebase";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 class Comentarios extends Component {
-    constructor() {
-      super();
-      this.state = {
-        liked: false,
-        cantlikes: ""
-      };
-    }
-  
-    render() {
-      return (
-        <View>
-            <Text>{this.props.infoComentarios.data.comentario}</Text>
-            <Text>Autor: {this.props.infoComentarios.data.email}</Text>
-        </View>
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false,
+      cantLikes: props.infoComentarios.data.likes
+        ? props.infoComentarios.data.likes.length
+        : 0,
+    };
+  }
+
+  componentDidMount() {
+    if (
+      this.props.infoComentarios.data.likes &&
+      this.props.infoComentarios.data.likes.includes(auth.currentUser.email)
+    ) {
+      this.setState({
+        liked: true,
+      });
     }
   }
 
-  export default Comentarios;
+  like() {
+    db.collection("comentarios")
+      .doc(this.props.infoComentarios.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email),
+      })
+      .then(() => {
+        this.setState((prevState) => ({
+          liked: true,
+          cantLikes: prevState.cantLikes + 1,
+        }));
+      });
+  }
+
+  unLike() {
+    db.collection("comentarios")
+      .doc(this.props.infoComentarios.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(
+          auth.currentUser.email
+        ),
+      })
+      .then(() => {
+        this.setState((prevState) => ({
+          liked: false,
+          cantLikes: prevState.cantLikes - 1,
+        }));
+      });
+  }
+
+  deleteComentario() {
+    db.collection("comentarios")
+      .doc(this.props.infoComentarios.id)
+      .delete()
+      .then(() => {
+        console.log("Comentario eliminado");
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const { email, comentario, createdAt } = this.props.infoComentarios.data;
+
+    return (
+      <View>
+        <Text>Autor: {email}</Text>
+        <Text>{comentario}</Text>
+        <Text>{new Date(createdAt).toLocaleString()}</Text>
+
+
+        {this.state.liked ? (
+          <TouchableOpacity
+            onPress={() => this.unLike()}
+          >
+            <AntDesign name="like1" size={24} color="black" />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            onPress={() => this.like()}
+          >
+            <AntDesign name="like2" size={24} color="black" />
+          </TouchableOpacity>
+        )}
+        <Text>Cantidad de likes: {this.state.cantLikes}</Text>
+
+      
+        <TouchableOpacity
+  
+          onPress={() => this.deleteComentario()}
+        >
+          <Text>Eliminar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
+export default Comentarios;
